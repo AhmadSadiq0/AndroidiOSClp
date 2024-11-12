0fadc97e103b329294605385ddf8bae84c2f38dd
Ahmad Sadiq <ahmadsadiq6278@gmail.com>
2024-11-12 22:50:32 +0500

    Updating App.tsx

--- App.tsx
+++ App.tsx
@@ -5,114 +5,156 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState, useEffect } from 'react';
+//Core components 
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Image,
+  ActivityIndicator,
+  FlatList,
+  Button,
+  Alert,
+  TextInput
 } from 'react-native';
+import Header from './src/components/HeaderClass'
+import Input from './src/components/Input';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  //useState => Hooks
+  //Why do we use useState to create a state
+  //What's a state?
+  //states are immutable
+  const [value, setValue] = useState(10)
+  const [number, setNumber] = useState(10)
+  const [num, setNum] = useState(101)
+
+  let count = 10
+  //JSX => Javascript xml
+  console.log('rendered')
+  const onButtonPressed = () => {
+    Alert.alert('Hello World')
+  }
+  const onAddPressed = () => {
+    setValue(value + 1)
+    // count++
+    // console.log(count)
+  }
+  const onChangeNumber = () => {
+    setNumber(number + 1)
+    // count++
+    // console.log(count)
+  }
+  const onChangeNum = () => {
+    setNum(num + 1)
+    // count++
+    // console.log(count)
+  }
+  //mounting, updating and unmounting
+  useEffect(() => {
+    alert('Welcome')
+    //api call
+
+    //unmounting
+    // return () => {
+    //   alert('Un mounting')
+    // }
+
+  }, [])
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Header title='Welcome' />
+        <View style={styles.row}>
+          <Text>{"Value " + value}</Text>
+          <Text>{"Number " + number}</Text>
+          <Text>{"Num is " + num}</Text>
+
         </View>
-      </ScrollView>
+
+
+
+        <Text>Hello Consultants</Text>
+        <Text>Welcome to React Native CLP!</Text>
+        {/* <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+        /> */}
+        <Input placeholder={'Enter your email'} />
+        <Input placeholder={"Enter your password"} />
+
+        <ActivityIndicator />
+        <ActivityIndicator
+          animating={true}
+          color={'red'}
+          size={'large'}
+        />
+
+        <Button
+          onPress={onAddPressed}
+          title="Add"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+          onPress={onChangeNumber}
+          title="Update Number"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+          onPress={onChangeNum}
+          title="Update Num"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Header title='Users' />
+
+        <FlatList
+          data={[{ firstName: 'Arpit', lastName: '', age: 26 },
+          { firstName: 'Daniel', lastName: '', age: 26 }]}
+          renderItem={({ item }) => {
+            return (
+              <Text style={styles.flatListItem}>{item.firstName}</Text>
+            )
+          }}
+          keyExtractor={item => item.firstName}
+        />
+
+        <Button
+          onPress={onButtonPressed}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
+  container: {
+    flex: 1,
+    backgroundColor: 'brown'
+  }, flatListItem: {
     fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    backgroundColor: '#d3d3d3',
+    height: 30,
+    margin: 5
+
+  }, row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 });
 
 export default App;
